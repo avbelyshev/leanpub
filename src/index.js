0b8bddfe40ec478b888436f25f3b971e9d142cb0
avbelyshev <avbelishev@gmail.com>
2020-07-22 01:08:18 +0300

    Install React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+import ReactDom from 'react-dom';
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    const { name } = this.props;
+    return <div>Hello, {name}!</div>;
+  }
+}
+
+ReactDom.render(
+  <div>
+    <Hello name='World' />
+    <Hello name='Mars' />
+    <Hello name='Earth' />
+  </div>,
+  document.getElementById('root')
+);
